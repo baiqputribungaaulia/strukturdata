e6331260b935ee8f10545bd7b7bf55be2996d722
baiqputribungaaulia <baiqputribungaaulia@gmail.com>
2026-04-21 19:01:37 +0800

    pert 1

---
+++ if-statement.py
@@ -0,0 +1,22 @@
+
+
+
+
+
+nama = input("Masukan nama anda: ")
+
+if nama == "edy":  # kondisi pertama
+    print("Selamat datang edy")
+    print("Dosen ganteng")
+
+elif nama == "syhasdik":  # kondisi kedua
+    print("Selamat datang syhasdik")
+
+elif nama == "bunga":  # kondisi ketiga
+    print("Selamat datang bunga")
+
+else:
+    print("Anda bukan dosen")
+    print("Anda tidak ganteng")
+
+print("Program selesai")
