71c81e2733220504f7f1cf7401bcc76411cd1456
baiqputribungaaulia <baiqputribungaaulia@gmail.com>
2026-04-21 19:02:43 +0800

    pert2

---
+++ Loop.py
@@ -0,0 +1,26 @@
+# perulangan (LOOP)
+# for kondisi :
+#     aksi
+
+angka = 1
+print(angka)
+
+angka = angka + 3
+print(angka)
+
+angka = angka + 3
+
+# list
+angka = [0, 1, 2, 3, 4, 5]
+for i in angka:
+    print("ini list")
+
+# range
+angka = range(10)
+for i in angka:
+    print(f"ini range {i}")
+
+# string
+kata = "pak edy ganteng"
+for i in kata:
+    print(i)
